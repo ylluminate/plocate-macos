8b065fcadff742c1575d54aed1e9a78511b142d7
ylluminate <ylluminate@gmail.com>
2026-03-02 01:59:58 -0500

    replace assert() with error logging in darwin compat layer

--- darwin_utils.cpp
+++ darwin_utils.cpp
@@ -1,18 +1,22 @@
-#undef NDEBUG
-#include <cassert>
-
 #include "darwin_utils.h"
 
+#include <cstdio>
 #include <sys/resource.h>
 
 void darwin_disable_dataless() {
-    assert(!setiopolicy_np(IOPOL_TYPE_VFS_MATERIALIZE_DATALESS_FILES, IOPOL_SCOPE_PROCESS, IOPOL_MATERIALIZE_DATALESS_FILES_OFF));
+	if (setiopolicy_np(IOPOL_TYPE_VFS_MATERIALIZE_DATALESS_FILES, IOPOL_SCOPE_PROCESS, IOPOL_MATERIALIZE_DATALESS_FILES_OFF)) {
+		fprintf(stderr, "plocate: setiopolicy_np(IOPOL_TYPE_VFS_MATERIALIZE_DATALESS_FILES) failed\n");
+	}
 }
 
 void darwin_set_throttling(bool enable_throttling) {
-    assert(!setiopolicy_np(IOPOL_TYPE_DISK, IOPOL_SCOPE_PROCESS, enable_throttling ? IOPOL_THROTTLE : IOPOL_IMPORTANT));
+	if (setiopolicy_np(IOPOL_TYPE_DISK, IOPOL_SCOPE_PROCESS, enable_throttling ? IOPOL_THROTTLE : IOPOL_IMPORTANT)) {
+		fprintf(stderr, "plocate: setiopolicy_np(IOPOL_TYPE_DISK, throttle) failed\n");
+	}
 }
 
 void darwin_set_atime_updates(bool enable_atime_updates) {
-    assert(!setiopolicy_np(IOPOL_TYPE_DISK, IOPOL_SCOPE_PROCESS, enable_atime_updates ? IOPOL_ATIME_UPDATES_DEFAULT : IOPOL_ATIME_UPDATES_OFF));
+	if (setiopolicy_np(IOPOL_TYPE_DISK, IOPOL_SCOPE_PROCESS, enable_atime_updates ? IOPOL_ATIME_UPDATES_DEFAULT : IOPOL_ATIME_UPDATES_OFF)) {
+		fprintf(stderr, "plocate: setiopolicy_np(IOPOL_TYPE_DISK, atime) failed\n");
+	}
 }
--- firmlinks_darwin.cpp
+++ firmlinks_darwin.cpp
@@ -1,6 +1,3 @@
-#undef NDEBUG
-#include <cassert>
-
 #include "firmlinks_darwin.h"
 #include "conf.h"
 
@@ -33,22 +30,34 @@ std::vector<FirmlinkPair> get_firmlinks(void) {
 	dev_buf[0] = '\0';
 	struct statfs *mounts = nullptr;
 	int num_mounts = getmntinfo_r_np(&mounts, 0);
-	assert(num_mounts > 0);
-	assert(mounts);
+	if (num_mounts <= 0 || !mounts) {
+		fprintf(stderr, "plocate: getmntinfo_r_np failed\n");
+		free(mounts);
+		return {};
+	}
 	for (int i = 0; i < num_mounts; ++i) {
 		if (memcmp(mounts[i].f_mntonname, "/", sizeof("/"))) {
 			continue;
 		}
 		const char  * const dev_path = mounts[i].f_mntfromname;
-		assert(!memcmp(dev_path, "/dev/disk", sizeof("/dev/disk") - 1));
+		if (memcmp(dev_path, "/dev/disk", sizeof("/dev/disk") - 1)) {
+			fprintf(stderr, "plocate: root device path does not start with /dev/disk: %s\n", dev_path);
+			free(mounts);
+			return {};
+		}
 		const char * const dev_name = dev_path + (sizeof("/dev/") - 1);
 		const size_t dev_name_len = strlen(dev_name);
 		const char * const dev_trailer = dev_name + (sizeof("disk") - 1);
 		const size_t dev_trailer_len = strlen(dev_trailer);
 		const char * const dev_trailer_nul = dev_trailer + dev_trailer_len;
-		assert(dev_trailer_len > 0);
+		if (dev_trailer_len == 0) {
+			fprintf(stderr, "plocate: unexpected root device name format: %s\n", dev_path);
+			free(mounts);
+			return {};
+		}
 		size_t num_s = 0;
 		const char *first_s = nullptr;
+		bool bad_char = false;
 		for (size_t j = 0; j < dev_trailer_len; ++j) {
 			const char c = dev_trailer[j];
 			if (c == 's') {
@@ -56,47 +65,80 @@ std::vector<FirmlinkPair> get_firmlinks(void) {
 					first_s = &dev_trailer[j];
 				}
 				++num_s;
-			} else {
-				assert(c >= '0' && c <= '9');
+			} else if (c < '0' || c > '9') {
+				bad_char = true;
+				break;
 			}
 		}
-		assert(num_s <= 2);
+		if (bad_char || num_s > 2) {
+			fprintf(stderr, "plocate: unexpected root device name format: %s\n", dev_path);
+			free(mounts);
+			return {};
+		}
 		if (num_s == 2) {
-			assert(dev_trailer_len >= 4); // sXsY
-			assert(first_s[1] != 's');
-			assert(dev_trailer[dev_trailer_len - 1] != 's');
+			if (dev_trailer_len < 4 || first_s[1] == 's' || dev_trailer[dev_trailer_len - 1] == 's') {
+				fprintf(stderr, "plocate: unexpected root device name format: %s\n", dev_path);
+				free(mounts);
+				return {};
+			}
 		} else if (num_s == 1) {
-			assert(dev_trailer_len >= 2); // sX
-			assert(first_s < dev_trailer_nul);
+			if (dev_trailer_len < 2 || first_s >= dev_trailer_nul) {
+				fprintf(stderr, "plocate: unexpected root device name format: %s\n", dev_path);
+				free(mounts);
+				return {};
+			}
 		}
 		if (num_s >= 1) {
-			assert(first_s);
+			if (!first_s) {
+				fprintf(stderr, "plocate: unexpected root device name format: %s\n", dev_path);
+				free(mounts);
+				return {};
+			}
 			const size_t dev_len = (sizeof("disk") - 1) + (dev_trailer_len - strlen(first_s));
-			assert(dev_len + 1 < sizeof(dev_buf));
+			if (dev_len + 1 >= sizeof(dev_buf)) {
+				fprintf(stderr, "plocate: root device name too long: %s\n", dev_path);
+				free(mounts);
+				return {};
+			}
 			memcpy(dev_buf, dev_name, dev_len);
 			dev_buf[dev_len] = '\0';
 		} else {
-			assert(dev_name_len + 1 < sizeof(dev_buf));
+			if (dev_name_len + 1 >= sizeof(dev_buf)) {
+				fprintf(stderr, "plocate: root device name too long: %s\n", dev_path);
+				free(mounts);
+				return {};
+			}
 			memcpy(dev_buf, dev_name, dev_name_len + 1);
 		}
 		break;
 	}
 	free(mounts);
 
-	assert(dev_buf[0] != '\0');
+	if (dev_buf[0] == '\0') {
+		fprintf(stderr, "plocate: could not determine root device\n");
+		return {};
+	}
 
 	char uuid_str[UUID_STR_BUF_LEN_W_NUL] = {0};
 	size_t uuid_str_sz = sizeof(uuid_str);
-	assert(!sysctlbyname("kern.bootuuid", uuid_str, &uuid_str_sz, nullptr, 0));
+	if (sysctlbyname("kern.bootuuid", uuid_str, &uuid_str_sz, nullptr, 0)) {
+		fprintf(stderr, "plocate: sysctlbyname(kern.bootuuid) failed\n");
+		return {};
+	}
 
 	uuid_t vol_group_id;
-	assert(!uuid_parse(uuid_str, vol_group_id));
+	if (uuid_parse(uuid_str, vol_group_id)) {
+		fprintf(stderr, "plocate: uuid_parse failed for boot uuid\n");
+		return {};
+	}
 
 	CFMutableArrayRef firmlinks = nullptr;
 	const OSStatus getfl_res =
 		APFSContainerVolumeGroupGetFirmlinks(dev_buf, vol_group_id, &firmlinks);
-	assert(!getfl_res);
-	assert(firmlinks);
+	if (getfl_res) {
+		fprintf(stderr, "plocate: APFSContainerVolumeGroupGetFirmlinks failed: %d\n", (int)getfl_res);
+		return {};
+	}
 	if (!firmlinks) {
 		if (conf_debug_pruning) {
 			fprintf(stderr, "Didn't find any firmlinks.\n");
@@ -105,16 +147,31 @@ std::vector<FirmlinkPair> get_firmlinks(void) {
 	}
 
 	const CFIndex numfl = CFArrayGetCount(firmlinks);
-	assert(numfl % 2 == 0);
-	assert(numfl > 0);
+	if (numfl % 2 != 0) {
+		fprintf(stderr, "plocate: firmlink array has odd count\n");
+		CFRelease(firmlinks);
+		return {};
+	}
+	if (numfl == 0) {
+		CFRelease(firmlinks);
+		return {};
+	}
 	FirmlinkPair tmp;
 	std::vector<FirmlinkPair> flplist(numfl/2 + 1);
 	for (CFIndex i = 0; i < numfl; ++i) {
 		CFStringRef val = reinterpret_cast<CFStringRef>(
 			CFArrayGetValueAtIndex(firmlinks, i));
-		assert(val);
+		if (!val) {
+			fprintf(stderr, "plocate: null entry in firmlink array\n");
+			CFRelease(firmlinks);
+			return {};
+		}
 		char path[1024];
-		assert(CFStringGetCString(val, path, sizeof(path), kCFStringEncodingUTF8));
+		if (!CFStringGetCString(val, path, sizeof(path), kCFStringEncodingUTF8)) {
+			fprintf(stderr, "plocate: CFStringGetCString failed for firmlink entry\n");
+			CFRelease(firmlinks);
+			return {};
+		}
 		if (i % 2 == 0) {
 			tmp.logical_path = path;
 		} else {
@@ -149,5 +206,7 @@ const std::string *is_firmlink_target(const std::string &path) {
 
 void firmlink_init() {
 	firmlink_list = get_firmlinks();
-	assert(firmlink_list.size() > 0);
+	if (firmlink_list.empty()) {
+		fprintf(stderr, "plocate: warning: no firmlinks found\n");
+	}
 }
